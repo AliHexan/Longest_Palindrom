47746c31ab43442a88aa8e8e2a4d1bc0607f1296
AliHexan <71488596+AliHexan@users.noreply.github.com>
2020-09-19 23:54:00 +0500

    Update l_palindrom.py

---
+++ l_palindrom.py
@@ -0,0 +1,37 @@
+
+def longestPalindrome(str):
+
+    start=0
+    length=len(str)
+    mx=1
+    for i in range(1,length):
+
+
+        low=i-1
+        high=i
+
+        while low>=0 and high<length and str[low]==str[high]:
+            if high-low+1>mx:
+                start=low
+                mx=high-low+1
+            low-=1
+            high+=1
+
+        low=i-1
+        high=i+1
+
+
+        while low>=0 and high<length and str[low]==str[high]:
+            if high-low+1>mx:
+                start=low
+                mx=high-low+1
+            low-=1
+            high+=1
+
+        
+    print(str[start:start+mx])
+
+    print(mx)
+
+
+longestPalindrome("forgeeksskeegfor")
